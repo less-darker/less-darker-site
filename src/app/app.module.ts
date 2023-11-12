2ea75e4d0fd0badf8e421849637f1d25c1e161d5
Fiskoal <knicleyjakob@gmail.com>
2023-11-12 03:49:54 -0500

    touch up site for beta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DownloadComponent } from './components/download/download.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SocialsComponent } from './components/socials/socials.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DownloadComponent,
-    HeaderComponent,
     FooterComponent,
-    SocialsComponent
+    SocialsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
